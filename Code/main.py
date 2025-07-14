5fabebc665acd39e535e3640149f005d6ec4f1c8
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-14 21:59:52 +0100

    FIX: removal of EnginePowerFactory import to have initialised inside the FrameworkInitialiser and everything run via the GlobalRegistry

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,11 @@
 from FrameworkInitialiser import FrameworkInitialiser
 # from Framework.Engine import EngineContainer
 from Framework import GlobalRegistry as gbl
-# from Framework.EnginePowerFactory import EnginePowerFactory
 
 if __name__ == "__main__":
     fw = FrameworkInitialiser()
     fw.initialize()
     gbl.Msg.OutputSplash()
+    gbl.Engine.activatePfNetwork(r"Personal_Learning\Load Flow")
+    gbl.Engine.activateStudyCase("Study Case")
+    print("Framework initialized and ready to use!")
