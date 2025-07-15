07eace5d81c214da33e9f74e9e1b7421d3512abe
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-15 21:14:37 +0100

    FEAT: creation of busbar component. Inherits ComponentBase and has methods that will only be valid depending on the level of study that has been done on the network

--- Code/ComponentDefinition.py
+++ Code/ComponentDefinition.py
@@ -40,14 +40,93 @@ class ComponentBaseTemplate:
         # This method should be overridden in subclasses to retrieve the status from the engine
         raise NotImplementedError("This method should be implemented in subclasses.")
     
-    def ExtractDataModelComponentFromEngine(self):
+    def GetDataModelComponentFromEngine(self):
         # This method should be overridden in subclasses to extract data from the engine
         raise NotImplementedError("This method should be implemented in subclasses.")
     
-    def LoadDataModelComponentToEngine(self):
+    def SetDataModelComponentToEngine(self):
         # This method should be overridden in subclasses to load data to the engine
         raise NotImplementedError("This method should be implemented in subclasses.")
     
+    
+    
+    class Busbar(ComponentBaseTemplate):
+        def __init__(self, BusID):
+            ComponentBaseTemplate.__init__(self)
+            try:
+                BusID = int(BusID)
+            except:
+                BusID = str(BusID)
+            self.BusID = BusID
+            self.name = ''
+            self.kV = 0.0
+            self.Branches = []
+            self.Generators = []
+            self.Loads = []
+            self.Type = 0
+            self.Area = 0
+            self.Owner = ''
+            self.Disconnected = False
+            self.Slack = False
+            
+            # post load flow attributes
+            self.puVoltage = 1.0
+            self.busbarAngle = 0.0
+            
+            # Engine model updater
+            self.BasicEngineModelUpdater = None
+            self.LoadFlowEngineModelUpdater = None
+            self.HarmonicEngineModelUpdater = None
+            self.ContingencyAnalysisEngineModelUpdater = None
+            
+            
+            
+        def GetDataModelComponentReadableName(self):
+            if str(self.BusID) != str(self.name):
+                return f"{self.name}-({self.BusID})"
+            else:
+                return str(self.BusID)
+         
+        def StatusToEngine(self):
+            # Logic to update the engine with the busbar status
+            bOK = False
+            if self.BasicEngineModelUpdater:
+                bOK = self.BasicEngineModelUpdater.UpdateBusbarStatus(self)
+            return bOK
+        
+        def GetDataModelComponentStatusFromEngine(self):
+            bOK = False
+            if self.BasicEngineModelUpdater:
+                bOK = self.BasicEngineModelUpdater.GetBusbarStatusFromEngine(self)
+            return bOK
+        
+        def GetDataModelComponentFromEngine(self):
+            bOK = False
+            if self.BasicEngineModelUpdater:
+                bOK = self.BasicEngineModelUpdater.GetBusbarFromEngine(self)
+            return bOK
+        
+        def SetDataModelComponentToEngine(self):
+            bOK = False
+            if self.BasicEngineModelUpdater:
+                bOK = self.BasicEngineModelUpdater.SetBusbarToEngine(self)
+            return bOK
+        
+        def GetLoadFlowResuts(self):
+            bOK = False
+            if self.LoadFlowEngineModelUpdater:
+                bOK = self.LoadFlowEngineModelUpdater.GetLoadFlowResults(self)
+            return bOK
+        def GetHarmonicResults(self):
+            bOK = False
+            if self.HarmonicEngineModelUpdater:
+                bOK = self.HarmonicEngineModelUpdater.GetHarmonicResults(self)
+            return bOK
+        def GetContingencyAnalysisResults(self):
+            bOK = False
+            if self.ContingencyAnalysisEngineModelUpdater:
+                bOK = self.ContingencyAnalysisEngineModelUpdater.GetContingencyAnalysisResults(self)
+            return bOK
     
     
     
