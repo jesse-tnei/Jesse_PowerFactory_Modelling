48d0c48a0aec5ad5c8e89a8d217063ce8170368c
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-19 12:28:25 +0100

    CHORE: renamed all methods to lowercase

--- Code/Framework/Engine.py
+++ Code/Framework/Engine.py
@@ -6,198 +6,199 @@ import string
 from Framework import GlobalRegistry as gbl
 #from FrameworkInitialiser import FrameworkInitialiser
 
+
 class EngineContainer:
+
     def __init__(self):
         self.msg = gbl.Msg
         self.m_active_network = None
         self.m_strTypeOfEngine = "NoEngine"
         self.m_strVersion = "1.0.0"
         self.m_strAuthor = "Unknown"
-        
+
         self.m_NetworkToDataModelInterface = None
         self.m_oNetworkStudySettings = None
         self.m_oNetworkLoadFlowRunInstanceInstance = None
         self.m_oNetworkHarmonicsRunInstanceInstance = None
         self.m_oNetworkShortCircuitRunInstance = None
         self.m_oNetworkFaultAnalysisRunInstance = None
-        
+
         #__________________________ENGINE INITIALIZATION________________________
-        def IsIPSA():
+        def isipsa():
             """initialises IPSA to false such that when inherited, the engine can set it to true if it is an IPSA engine"""
             return False
-        def IsPowerFactory():
+
+        def ispowerfactory():
             """initialises PowerFactory to false such that when inherited, the engine can set it to true if it is a PowerFactory engine"""
             return False
-        def IsOpenDSS():
+
+        def isopendss():
             """initialises OpenDSS to false such that when inherited, the engine can set it to true if it is an OpenDSS engine"""
             return False
-        
-        
+
         #__________________________ENGINE VERSION INFORMATION________________________
-        def GetVersion(self):
+        def getversion(self):
             """Returns the version of the engine"""
             return self.m_strVersion
-        def GetAuthor(self):
+
+        def getauthor(self):
             """Returns the author of the engine"""
             return self.m_strAuthor
-        def GetTypeOfEngine(self):
+
+        def gettypeofengine(self):
             """Returns the type of engine"""
             return self.m_strTypeOfEngine
-        
-        
+
         #__________________________GENERIC ENGINE INTERFACE METHODS________________________
-        def ImportNetworkModel(self, strFileName):
+        def importnetworkmodel(self, strFileName):
             """Imports the network model from the specified file"""
-            self.msg.AddError("ImportNetworkModel not implemented in base class")
+            self.msg.adderror("ImportNetworkModel not implemented in base class")
             return False
-        
-        def ExportNetworkModel(self, strFileName):
+
+        def exportnetworkmodel(self, strFileName):
             """Exports the network model to the specified file"""
-            self.msg.AddError("ExportNetworkModel not implemented in base class")
+            self.msg.adderror("ExportNetworkModel not implemented in base class")
             return False
-        
-        def OpenNetwork(self, strNetworkName):
+
+        def opennetwork(self, strNetworkName):
             """Opens the specified network"""
-            self.msg.AddError("OpenNetwork not implemented in base class")
+            self.msg.adderror("OpenNetwork not implemented in base class")
             return False
-        
-        def CloseNetwork(self):
+
+        def closenetwork(self):
             """Closes the currently open network"""
-            self.msg.AddError("CloseNetwork not implemented in base class")
+            self.msg.adderror("CloseNetwork not implemented in base class")
             return False
-        
-        
+
         #__________________________GENERIC ENGINE STUDY TYPE METHODS________________________
-        def RunLoadFlow(self, strNetworkName):
+        def runloadflow(self, strNetworkName):
             """Runs a load flow analysis on the specified network"""
-            self.msg.AddError("RunLoadFlow not implemented in base class")
+            self.msg.adderror("RunLoadFlow not implemented in base class")
             return False
-        
-        def RunHarmonics(self, strNetworkName):
+
+        def runharmonics(self, strNetworkName):
             """Runs a harmonics analysis on the specified network"""
-            self.msg.AddError("RunHarmonics not implemented in base class")
+            self.msg.adderror("RunHarmonics not implemented in base class")
             return False
-        
-        def RunShortCircuit(self, strNetworkName):
+
+        def runshortcircuit(self, strNetworkName):
             """Runs a short circuit analysis on the specified network"""
-            self.msg.AddError("RunShortCircuit not implemented in base class")
+            self.msg.adderror("RunShortCircuit not implemented in base class")
             return False
-        def RunFaultAnalysis(self, strNetworkName):
+
+        def runfaultanalysis(self, strNetworkName):
             """Runs a fault analysis on the specified network"""
-            self.msg.AddError("RunFaultAnalysis not implemented in base class")
+            self.msg.adderror("RunFaultAnalysis not implemented in base class")
             return False
-        
-        
+
         #__________________________GENERIC ENGINE DATA MODEL METHODS________________________
-        def CheckNetworkOpen(self):
+        def checknetworkopen(self):
             """Checks if a network is currently open"""
             if self.m_active_network is None:
-                self.msg.AddError("No network is currently open")
+                self.msg.adderror("No network is currently open")
                 return False
             return True
-        
-        def TransferDataFromNetworkToDataModel(self):
+
+        def transferdatafromnetworktodatamodel(self):
             """Transfers data from the active network to the data model"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                bOK = self.m_NetworkToDataModelInterface.TransferDataFromNetworkToDataModel()
-            
+                bOK = self.m_NetworkToDataModelInterface.transferdatafromnetworktodatamodel()
+
             return bOK
-        
-        def TransferDataFromDataModelToNetwork(self):
+
+        def transferdatafromdatamodeltonetwork(self):
             """Transfers data from the data model to the active network"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                bOK = self.m_NetworkToDataModelInterface.TransferDataFromDataModelToNetwork()
+                bOK = self.m_NetworkToDataModelInterface.transferdatafromdatamodeltonetwork()
             return bOK
-        
-        def GetBusbarsFromNetwork(self):
+
+        def getbusbarsfromnetwork(self):
             """Retrieves busbars from the engine"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetBusbarsFromNetwork()
+                return self.m_oNetworkToDataModelInterface.getbusbarsfromnetwork()
             return bOK
-        
-        def GetBranchesFromNetwork(self):
+
+        def getbranchesfromnetwork(self):
             """Retrieves branches from the engine"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetBranchesFromNetwork()
+                return self.m_oNetworkToDataModelInterface.getbranchesfromnetwork()
             return bOK
-        
-        def GetGeneratorsFromNetwork(self):
+
+        def getgeneratorsfromnetwork(self):
             """Retrieves generators from the engine"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetGeneratorsFromNetwork()
+                return self.m_oNetworkToDataModelInterface.getgeneratorsfromnetwork()
             return bOK
-        
-        def GetLoadsFromNetwork(self):
+
+        def getloadsfromnetwork(self):
             """Retrieves loads from the engine"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetLoadsFromNetwork()
+                return self.m_oNetworkToDataModelInterface.getloadsfromnetwork()
             return bOK
-        
-        def AddGeneratorToNetwork(self, generator_data):
+
+        def addgeneratortonetwork(self, generator_data):
             """Adds a generator to the network"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.AddGeneratorToNetwork(generator_data)
+                return self.m_oNetworkToDataModelInterface.addgeneratortonetwork(generator_data)
             return bOK
-        
-        def AddLoadToNetwork(self, load_data):
+
+        def addloadtonetwork(self, load_data):
             """Adds a load to the network"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.AddLoadToNetwork(load_data)
+                return self.m_oNetworkToDataModelInterface.addloadtonetwork(load_data)
             return bOK
-        
-        def SetNetworkBusbarValues(self, dmBusbar):
+
+        def setnetworkbusbarvalues(self, dmBusbar):
             """Sets busbar values in the network"""
-            bOK = self.CheckNetworkOpen()
+            bOK = self.checknetworkopen()
             if bOK:
-                return self.m_oNetworkToDataModelInterface.SetNetworkBusbarValues(dmBusbar)
+                return self.m_oNetworkToDataModelInterface.setnetworkbusbarvalues(dmBusbar)
             return bOK
-        
-        
-        
+
         #__________________________ANALYSIS RESULTS METHODS________________________
-        def GetLoadFlowResults(self):
+        def getloadflowresults(self):
             """Retrieves load flow results from the engine"""
-            bOK = self.CheckNetworkOpen() and self.m_oNetworkLoadFlowRunInstanceInstance
+            bOK = self.checknetworkopen() and self.m_oNetworkLoadFlowRunInstanceInstance
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetLoadFlowResults()
-            self.msg.AddError("Load flow results not available")
+                return self.m_oNetworkToDataModelInterface.getloadflowresults()
+            self.msg.adderror("Load flow results not available")
             return bOK
-        
-        def GetHarmonicsResults(self):
+
+        def getharmonicsresults(self):
             """Retrieves harmonics results from the engine"""
-            bOK = self.CheckNetworkOpen() and self.m_oNetworkHarmonicsRunInstanceInstance
+            bOK = self.checknetworkopen() and self.m_oNetworkHarmonicsRunInstanceInstance
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetHarmonicsResults()
-            self.msg.AddError("Harmonics results not available")
+                return self.m_oNetworkToDataModelInterface.getharmonicsresults()
+            self.msg.adderror("Harmonics results not available")
             return bOK
-        
-        def GetShortCircuitResults(self):
+
+        def getshortcircuitresults(self):
             """Retrieves short circuit results from the engine"""
-            bOK = self.CheckNetworkOpen() and self.m_oNetworkShortCircuitRunInstanceInstance
+            bOK = self.checknetworkopen() and self.m_oNetworkShortCircuitRunInstanceInstance
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetShortCircuitResults()
-            self.msg.AddError("Short circuit results not available")
+                return self.m_oNetworkToDataModelInterface.getshortcircuitresults()
+            self.msg.adderror("Short circuit results not available")
             return bOK
-        
-        def GetFaultAnalysisResults(self):
+
+        def getfaultanalysisresults(self):
             """Retrieves fault analysis results from the engine"""
-            bOK = self.CheckNetworkOpen() and self.m_oNetworkFaultAnalysisRunInstance
+            bOK = self.checknetworkopen() and self.m_oNetworkFaultAnalysisRunInstance
             if bOK:
-                return self.m_oNetworkToDataModelInterface.GetFaultAnalysisResults()
-            self.msg.AddError("Fault analysis results not available")
+                return self.m_oNetworkToDataModelInterface.getfaultanalysisresults()
+            self.msg.adderror("Fault analysis results not available")
             return bOK
-        
+
+
 # if __name__ == "__main__":
 #     fw = FrameworkInitialiser()
 #     fw.initialize()
 #     enginebasetemplateinstance = EngineContainer()
 #     enginebasetemplateinstance.msg.OutputSplash()
-    
