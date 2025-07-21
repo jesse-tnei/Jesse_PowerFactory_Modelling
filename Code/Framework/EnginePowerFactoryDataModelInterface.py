0c405e3f09dffb7cafb6246fceedc50d3c30f790
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-21 19:43:10 +0100

    FEAT: added methods to get line data, create line objects inside the DataModelManager and associate the created lines with corresponding busbars

--- Code/Framework/EnginePowerFactoryDataModelInterface.py
+++ Code/Framework/EnginePowerFactoryDataModelInterface.py
@@ -9,6 +9,7 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
         self.load_dictionary = {}  # Dictionary to hold load IDs and their corresponding bus IDs
         
         self.generator_dictionary = {}  # Dictionary to hold generator IDs and their corresponding bus IDs
+        self.branch_dictionary = {}  # Dictionary to hold branch IDs and their corresponding bus IDs
         
         
      
@@ -45,7 +46,7 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
             current = current.GetParent()
             
         if current:
-            terminal_id = f"{current.GetAttribute('loc_name')}_{current.GetAttribute('uknom')}"
+            terminal_id = f"{current.cpSubstat.GetAttribute('loc_name')}_{current.GetAttribute('loc_name')}_{current.GetAttribute('uknom')}"
             return terminal_id
         
     def getbusbarvaluesfromnetwork(self, busbar):
@@ -69,6 +70,61 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
         return bOK
         
 #____________________BRANCH METHODS________________________#
+    def getbranchesfromnetwork(self):
+            """Retrieves branches from the PowerFactory network."""
+            bOK = True
+            if bOK:
+                bOK = self.get_linesfromnetwork()
+            if bOK:
+                bOK = self.get_transformersfromnetwork()
+            return bOK
+
+    def get_linesfromnetwork(self):
+        """Retrieves lines from the PowerFactory network."""
+        bOK = True
+        if bOK:
+            lines = gbl.Engine.m_pFApp.GetCalcRelevantObjects("ElmLne")
+            gbl.Msg.AddRawMessage(f"Total lines retrieved successfully from the network: {len(lines)}")
+            for line in lines:
+                line_id = line.GetAttribute("loc_name")
+                self.branch_dictionary[line_id] = line
+                terminal1 = line.GetAttribute("bus1")
+                terminal2 = line.GetAttribute("bus2")
+                if terminal1 and terminal2:
+                    bus1_id = self._standardize_terminal_id(terminal1)
+                    bus2_id = self._standardize_terminal_id(terminal2)
+                    if bus1_id and bus2_id and bus1_id in self.terminal_dictionary and bus2_id in self.terminal_dictionary:
+                        line_datamodel = gbl.DataFactory.createbranch(bus1_id, bus2_id, 0, line_id)
+                        if line_datamodel is None:
+                            gbl.Msg.AddError(f"Failed to create line for terminals {bus1_id} and {bus2_id}.")
+                            continue
+                        if not self.getlinevaluesfromnetwork(line_datamodel):
+                            gbl.Msg.AddError(f"Failed to retrieve values for line {line_datamodel.BranchID}.")
+                            continue
+                        gbl.DataModelManager.Branch_TAB.append(line_datamodel)
+        gbl.Msg.AddRawMessage(f"Total lines added to DataModel: {len(gbl.DataModelManager.Branch_TAB)}")
+        return bOK
+
+    def getlinevaluesfromnetwork(self, line_datamodel):
+        """Retrieves line values from the PowerFactory network."""
+        bOK = True
+        if bOK:
+            line_obj = self.branch_dictionary.get(line_datamodel.BranchID)
+            if line_obj:
+                name = line_obj.GetAttribute("loc_name")
+                ON = not(bool(line_obj.GetAttribute("outserv")))
+
+                if name is None or ON is None:
+                    gbl.Msg.AddError(f"Failed to retrieve values for line {line_datamodel.BranchID}.")
+                    bOK = False
+                else:
+                    line_datamodel.name = name
+                    line_datamodel.ON = ON
+
+        return bOK
+    
+    def get_transformersfromnetwork(self):
+        pass
 
 #____________________LOAD METHODS________________________#
 
@@ -85,24 +141,24 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
                 if terminal:
                     bus_id = self._standardize_terminal_id(terminal)
                     if bus_id and bus_id in self.terminal_dictionary:
-                        load_data = gbl.DataFactory.createload(bus_id, load_id)
-                        if load_data is None:
+                        load_item = gbl.DataFactory.createload(bus_id, load_id)
+                        if load_item is None:
                             gbl.Msg.AddError(f"Failed to create load for terminal {bus_id}.")
                             continue
-                        if not self.getloadvaluesfromnetwork(load_data):
-                            gbl.Msg.AddError(f"Failed to retrieve values for load {load_data.LoadID}.")
+                        if not self.getloadvaluesfromnetwork(load_item):
+                            gbl.Msg.AddError(f"Failed to retrieve values for load {load_item.LoadID}.")
                             continue
-                        if not gbl.DataModelManager.addloadtotab(load_data):
-                            gbl.Msg.AddError(f"Failed to add load {load_data.LoadID} to DataModel.")
+                        if not gbl.DataModelManager.addloadtotab(load_item):
+                            gbl.Msg.AddError(f"Failed to add load {load_item.LoadID} to DataModel.")
                             continue
         gbl.Msg.AddRawMessage(f"Total loads added to DataModel: {len(gbl.DataModelManager.Load_TAB)}")
         return bOK
     
-    def getloadvaluesfromnetwork(self, load):
+    def getloadvaluesfromnetwork(self, load_item):
         """Retrieves load values from the PowerFactory network."""
         bOK = True
         if bOK:
-            load_obj = self.load_dictionary.get(load.LoadID)
+            load_obj = self.load_dictionary.get(load_item.LoadID)
             if load_obj:
                 name = load_obj.GetAttribute("loc_name")
                 P = load_obj.GetAttribute("plini")
@@ -110,13 +166,13 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
                 ON = not(bool(load_obj.GetAttribute("outserv")))
                 
                 if name is None or P is None or Q is None or ON is None:
-                    gbl.Msg.AddError(f"Failed to retrieve values for load {load.LoadID}.")
+                    gbl.Msg.AddError(f"Failed to retrieve values for load {load_item.LoadID}.")
                     bOK = False
                 else:
-                    load.name = name
-                    load.MW = P
-                    load.MVar = Q
-                    load.ON = ON
+                    load_item.name = name
+                    load_item.MW = P
+                    load_item.MVar = Q
+                    load_item.ON = ON
         return bOK
     
     
@@ -134,24 +190,24 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
                 if terminal:
                     bus_id = self._standardize_terminal_id(terminal)
                     if bus_id and bus_id in self.terminal_dictionary:
-                        gen_data = gbl.DataFactory.creategenerator(bus_id, gen_id)
-                        if gen_data is None:
+                        gen_item = gbl.DataFactory.creategenerator(bus_id, gen_id)
+                        if gen_item is None:
                             gbl.Msg.AddError(f"Failed to create generator for terminal {bus_id}.")
                             continue
-                        if not self.getgeneratorvaluesfromnetwork(gen_data):
-                            gbl.Msg.AddError(f"Failed to retrieve values for generator {gen_data.GenID}.")
+                        if not self.getgeneratorvaluesfromnetwork(gen_item):
+                            gbl.Msg.AddError(f"Failed to retrieve values for generator {gen_item.GenID}.")
                             continue
-                        if not gbl.DataModelManager.addgentotab(gen_data):
-                            gbl.Msg.AddError(f"Failed to add generator {gen_data.GenID} to DataModel.")
+                        if not gbl.DataModelManager.addgentotab(gen_item):
+                            gbl.Msg.AddError(f"Failed to add generator {gen_item.GenID} to DataModel.")
                             continue
         gbl.Msg.AddRawMessage(f"Total generators added to DataModel: {len(gbl.DataModelManager.Gen_TAB)}")
         return bOK
     
-    def getgeneratorvaluesfromnetwork(self, generator):
+    def getgeneratorvaluesfromnetwork(self, gen_item):
         """Retrieves generator values from the PowerFactory network."""
         bOK = True
         if bOK:
-            gen_obj = self.generator_dictionary.get(generator.GenID)
+            gen_obj = self.generator_dictionary.get(gen_item.GenID)
             if gen_obj:
                 name = gen_obj.GetAttribute("loc_name")
                 P = gen_obj.GetAttribute("pgini")
@@ -163,16 +219,16 @@ class EnginePowerFactoryDataModelInterface(EngineDataModelInterfaceContainer):
                 Qmin = gen_obj.GetAttribute("cQ_min")
                 
                 if any(x is None for x in [name, P, Q, ON, pf, Pnom, Qmax, Qmin]):
-                    gbl.Msg.AddError(f"Failed to retrieve values for generator {generator.GeneratorID}.")
+                    gbl.Msg.AddError(f"Failed to retrieve values for generator {gen_item.GenID}.")
                     bOK = False
                 else:
-                    generator.name = name
-                    generator.MW = P
-                    generator.MVar = Q
-                    generator.ON = ON
-                    generator.pf = pf
-                    generator.MWCapacity = Pnom
-                    generator.Qmax = Qmax
-                    generator.Qmin = Qmin
+                    gen_item.name = name
+                    gen_item.MW = P
+                    gen_item.MVar = Q
+                    gen_item.ON = ON
+                    gen_item.pf = pf
+                    gen_item.MWCapacity = Pnom
+                    gen_item.Qmax = Qmax
+                    gen_item.Qmin = Qmin
         return bOK
                 
