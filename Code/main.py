ba04ef07e15162ddbff63b3e389db33b0d66240a
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-26 15:18:59 +0100

    CHORE: tested working of the new methods through main.py

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,8 @@
+#python imports
 import os, sys
 
+#file imports
+# Ensure the parent directory is in the system path to import modules correctly
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              "Framework"))  #add Framework directory to path
@@ -7,6 +10,7 @@ from Code import FrameworkInitialiser as f_init
 from Code import GlobalEngineRegistry as gbl
 
 
+#main driver function to initialize the framework and run the load flow
 if __name__ == "__main__":
     fw = f_init.FrameworkInitialiser()
     fw.initialize()
@@ -15,6 +19,7 @@ if __name__ == "__main__":
     gbl.EngineContainer.activatepowerfactorystudycase("Study Case")
     gbl.DataModelInterfaceContainer.passelementsfromnetworktodatamodelmanager()
     gbl.EngineLoadFlowContainer.runloadflow()
+    gbl.EngineLoadFlowContainer.getandupdatebusbarloadflowresults()
+    gbl.EngineLoadFlowContainer.getandupdatelineloadflowresults()
 
-    
     print("Framework initialized and ready to use!")
