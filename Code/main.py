e72cda91439616aa298be6dc229edaa87959e615
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-26 13:05:28 +0100

    CHORE: tested and confirmed execution of loadflow command to confirm correct file setup and linkages and method implementation

--- Code/main.py
+++ Code/main.py
@@ -6,13 +6,15 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
 from Code import FrameworkInitialiser as f_init
 from Code import GlobalEngineRegistry as gbl
 
+
 if __name__ == "__main__":
     fw = f_init.FrameworkInitialiser()
     fw.initialize()
     gbl.Msg.DisplayWelcomeMessage()
-    gbl.EngineContainer.activatepowerfactorynetwork(r"Personal_Learning\Load Flow")
+    gbl.EngineContainer.activatepowerfactorynetwork(r"Personal_Learning\Load Flow [Again]")
     gbl.EngineContainer.activatepowerfactorystudycase("Study Case")
-    gbl.DataModelInterface.passelementsfromnetworktodatamodelmanager()
+    gbl.DataModelInterfaceContainer.passelementsfromnetworktodatamodelmanager()
+    gbl.EngineLoadFlowContainer.runloadflow()
 
     
     print("Framework initialized and ready to use!")
