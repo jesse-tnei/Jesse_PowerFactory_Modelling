44eb88b7a84dab589ade4435c8d0ced2e66f1675
jesse-tnei <144026208+jesse-tnei@users.noreply.github.com>
2025-07-28 10:46:44 +0100

    FEAT: tested to confirm if the StudyFileSettings works when driven from main.py

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,12 @@
 #python imports
 import os, sys
 
-#file imports
 # Ensure the parent directory is in the system path to import modules correctly
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             "Framework"))  #add Framework directory to path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Framework"))
+#framework imports
 from Code import FrameworkInitialiser as f_init
 from Code import GlobalEngineRegistry as gbl
-
 
 #main driver function to initialize the framework and run the load flow
 if __name__ == "__main__":
@@ -18,8 +16,12 @@ if __name__ == "__main__":
     gbl.EngineContainer.activatepowerfactorynetwork(r"Personal_Learning\Load Flow [Again]")
     gbl.EngineContainer.activatepowerfactorystudycase("Study Case")
     gbl.DataModelInterfaceContainer.passelementsfromnetworktodatamodelmanager()
-    gbl.EngineLoadFlowContainer.runloadflow()
-    gbl.EngineLoadFlowContainer.getandupdatebusbarloadflowresults()
-    gbl.EngineLoadFlowContainer.getandupdatelineloadflowresults()
 
-    print("Framework initialized and ready to use!")
+    if gbl.StudySettingsContainer.DoLoadFlow:
+        gbl.EngineLoadFlowContainer.runloadflow()
+        gbl.EngineLoadFlowContainer.getandupdatebusbarloadflowresults()
+        gbl.EngineLoadFlowContainer.getandupdatelineloadflowresults()
+        gbl.EngineLoadFlowContainer.getandupdatetransformerflowresults()
+        gbl.EngineLoadFlowContainer.getandupdateloadflowgeneratorresults()
+        gbl.EngineLoadFlowContainer.getandupdateloadsloadflowresults()
+        #gbl.EngineLoadFlowContainer.getloadflowresultsdiagramfromnetwork()
